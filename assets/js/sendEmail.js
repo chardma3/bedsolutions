6ac34d974818f5eee1c55ac93281de6c9665935d
chardma3 <chardma3@hotmail.com>
2021-01-10 18:35:21 +0000

    added emailjs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+function sendMail(contactForm) {
+    emailjs.send("bs_id", "bs", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false; // To block from loading a new page
+}
+
