3ac79f9f43aad0db41692ea84f03cbc59fb0178a
chardma3 <chardma3@hotmail.com>
2021-03-15 20:14:23 +0000

    fixed form as per docs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,18 +1,13 @@
-function sendMail(contactForm) {
-    emailjs.send("bs_id", "bs", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
+window.onload = function() {
+            document.getElementById('bs').addEventListener('submit', function(event) {
+                event.preventDefault();
+                const form = document.getElementById('bs');
+                emailjs.sendForm('bs_id', 'bs', this)
+                    .then(function() {
+                        console.log('SUCCESS!');
+                        form.reset();
+                    }, function(error) {
+                        console.log('FAILED...', error);
+                    });
+            });
         }
-    );
-    /*
-    return false; // To block from loading a new page
-}
-/*
